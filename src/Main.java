8361d466a61cb74cf73ced400a21cc293c36a5a8
SlaveToTheDark <milan_statev@yahoo.co.uk>
2024-04-17 13:20:50 +0300

    proper format of main

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         BoardItem item = new BoardItem("Registration doesn't work", LocalDate.now().plusDays(2));
-
         item.advanceStatus();
-
         BoardItem anotherItem = new BoardItem("Encrypt user data", LocalDate.now().plusDays(10));
-
-
 
         Board board = new Board();
 
-
-
         board.getItems().add(item);
-
         board.getItems().add(anotherItem);
 
-
-
         for (BoardItem boardItem : board.getItems()) {
-
             boardItem.advanceStatus();
-
         }
 
-
-
         for (BoardItem boardItem : board.getItems()) {
-
             System.out.println(boardItem.viewInfo());
-
         }
     }
 }
