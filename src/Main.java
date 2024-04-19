6134495b98c487b9aba0660e0ffae1d18835dccd
SlaveToTheDark <milan_statev@yahoo.co.uk>
2024-04-19 09:29:47 +0300

    learn to merge

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,12 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         BoardItem item1 = new BoardItem("Implement login/logout", LocalDate.now().plusDays(3));
-
         BoardItem item2 = new BoardItem("Secure admin endpoints", LocalDate.now().plusDays(5));
-
-
 
         Board board = new Board();
 
-
-
         board.addItem(item1); // add item1
-
         board.addItem(item2); // add item2
-
-       board.addItem(item1); // Exception thrown: Item already in the list
-
-
 
         System.out.println(board.totalItems()); // count: 2
     }
